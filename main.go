0d66d318de565f168dc18be64bf102aed5e026b8
cg <783141831@qq.com>
2020-10-28 12:19:13 +0800

    SetHash,

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 type Block struct {
 	PrevBlockHash []byte //前区块哈希
@@ -17,13 +20,61 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 		Hash:          []byte{}, //先填充为空，后续会填充数据
 		Data:          []byte(data),
 	}
+
+	block.SetHash()
+
 	return &block
+}
+
+//为了生成区块哈希，实现一个简单的函数，来计算哈希值，没有随机值，没有难度值
+func (block *Block) SetHash() {
+	var data []byte
+	data = append(data, block.PrevBlockHash...)
+	data = append(data, block.Data...)
+
+	hash /*[32]byte*/ := sha256.Sum256(data)
+	block.Hash = hash[:]
+}
+
+//创建区块链，使用Block数组模拟
+type BlockChain struct {
+	Blocks []*Block
+}
+
+//实现创建区块链方法
+func NewBlockChain() *BlockChain {
+	//在创建的时候添加一个区块：创世块
+	genesisBlock := NewBlock(genesisInfo, []byte{0x0000000000000000})
+
+	bc := BlockChain{Blocks: []*Block{genesisBlock}}
+	return &bc
+
+}
+
+//添加区块
+
+func (bc *BlockChain) AddBlock(data string) {
+	//1.创建一个区块
+
+	//bc.Blocks的最后一个区块的Hash值就是当前新去爱的PrevBlockHash
+	lastBlock := bc.Blocks[len(bc.Blocks)-1]
+	prevHash := lastBlock.Hash
+
+	block := NewBlock(data, prevHash)
+	//2.添加到bc.Blocks数组中
+	bc.Blocks = append(bc.Blocks, block)
 }
 
 func main() {
 	//fmt.Printf("hello world")
-	block := NewBlock(genesisInfo, []byte{0x0000000000000000})
-	fmt.Printf("PrevBlockHash:%x\n", block.PrevBlockHash)
-	fmt.Printf("Hash:%x\n", block.Hash)
-	fmt.Printf("Data:%s\n", block.Data)
+	//block := NewBlock(genesisInfo, []byte{0x0000000000000000})
+	bc := NewBlockChain()
+	bc.AddBlock("this is sec block")
+
+	for i, block := range bc.Blocks {
+		fmt.Printf("++++++++++++%d+++++++++++++++++\n", i)
+		fmt.Printf("PrevBlockHash:%x\n", block.PrevBlockHash)
+		fmt.Printf("Hash:%x\n", block.Hash)
+		fmt.Printf("Data:%s\n", block.Data)
+	}
 }
